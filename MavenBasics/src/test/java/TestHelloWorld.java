98012182d6033460197795e3a9f77fa9b2eec22f
Ekta <ektashaw35@gmail.com>
2024-08-02 23:18:06 +0530

    Adding 1st Basic Example

---
+++ MavenBasics/src/test/java/TestHelloWorld.java
@@ -0,0 +1,8 @@
+public class TestHelloWorld {
+    public void testHello(){
+        System.out.println("Test - Hello");
+    }
+    public void testWorld(){
+        System.out.println("Test - World");
+    }
+}
