6f0b1fb0dd0128136f047fb6eeb493c8b65e392c
Ekta <ektashaw35@gmail.com>
2024-08-09 19:37:28 +0530

    Refreshed

--- MavenBasics/src/test/java/TestHelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class TestHelloWorld {
-    public void testHello(){
-        System.out.println("Test - Hello");
-    }
-    public void testWorld(){
-        System.out.println("Test - World");
-    }
-}
--- mapStructPoject/src/main/java/UserMapper.java
+++
@@ -1,7 +0,0 @@
-import org.mapstruct.Mapper;
-
-@Mapper
-public interface UserMapper {
-    UserCommand userToCommand (User user);
-    User commandToUser(UserCommand userCmd);
-}
