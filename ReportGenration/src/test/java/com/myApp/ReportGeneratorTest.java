fed4511e2ef827b91ded236c050aad70af02b87f
Ekta <ektashaw35@gmail.com>
2024-08-13 16:44:17 +0530

    Adding Testing Report Genartor using Surefire Site plugin

---
+++ ReportGenration/src/test/java/com/myApp/ReportGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.myApp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportGeneratorTest {
+
+    @Test
+    void getReportTest() {
+        System.out.println("From test report");
+    }
+}
