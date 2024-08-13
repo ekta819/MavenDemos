34abc0ae3c06bc0b088f2a992d1fda5b200b5881
Ekta <ektashaw35@gmail.com>
2024-08-13 17:50:31 +0530

    Adding Integration Test Plugin (Failsafe) and SpotsBug reporting plugin

---
+++ IntegrationTestFailsafe/src/main/java/com/myApp/IntegrationFailsafe.java
@@ -0,0 +1,8 @@
+package com.myApp;
+
+public class IntegrationFailsafe {
+    public String failsafe()
+    {
+        return "From failsafe main";
+    }
+}
---
+++ IntegrationTestFailsafe/src/test/java/com/myApp/IntegrationFailsafeTest.java
@@ -0,0 +1,13 @@
+package com.myApp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegrationFailsafeTest {
+
+    @Test
+    void failsafe() {
+        System.out.println("From failsafe Test");
+    }
+}
