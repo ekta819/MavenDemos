b1cd12bf23272504df20297c8dfb8c9d589ab99a
Ekta <ektashaw35@gmail.com>
2024-08-22 11:28:16 +0530

    Added first multimodule exmaple

---
+++ MultiModule1/jap-entities/src/main/java/com/myApp/User.java
@@ -0,0 +1,21 @@
+package com.myApp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+public class User {
+    @Id
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
